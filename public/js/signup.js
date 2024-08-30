20ff0ad7042908fb2c86c4e7f3ef524febf78d4e
Lain <ckisielius@gmail.com>
2024-08-30 18:55:57 -0400

    added user signup frontend js

---
+++ public/js/signup.js
@@ -0,0 +1,27 @@
+// creates a new user using user input
+const signupHandler = async (event) => {
+  event.preventDefault();
+
+  const username = document.querySelector("#createUsername").value.trim();
+  const password = document.querySelector("#createPassword").value.trim();
+
+  if (username && password) {
+    const response = await fetch("api/users", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      // relocate user to homepage
+      document.location.replace("/homepage");
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+// event listener for sign up button
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupHandler);
